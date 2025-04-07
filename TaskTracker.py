54ef21fdd84b07c3e0e782fc60c1b6c4e6a0b752
Elyas Khoubach <elyaskh04@gmail.com>
2025-04-07 13:26:20 -0400

    My basic code

---
+++ TaskTracker.py
@@ -0,0 +1,26 @@
+import datetime
+
+tasks = []
+taskTracker = {}
+
+def saveTask(task):
+    tasks.append(task)
+    
+    
+def printTasks():
+    i = 1
+    for task in tasks:
+        print(i,'. ', task)
+        i += 1
+
+
+while True:
+    userTask = input("Enter a task: ") 
+    if userTask == 'q' or userTask == 'quit':
+        break   
+    saveTask(userTask)
+    time = datetime.datetime.today()
+    taskTracker.update({f"{time.strftime("%x")}: ": tasks})
+    
+printTasks()  
+print(taskTracker)
