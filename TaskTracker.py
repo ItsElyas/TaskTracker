5f21ff720cf0e1ac6e8c14792c89c9f4bc2ac5f9
Elyas Khoubach <elyaskh04@gmail.com>
2025-04-07 17:00:02 -0400

    Made a new branch to try to add switch case statemnts for multiple differnt user options

--- TaskTracker.py
+++ TaskTracker.py
@@ -25,10 +25,14 @@ def saveDict(tasks):
     
 def printTasks():
     i = 1
+    print("-------Tasks-------")
     for task in tasks:
-        print(i,'. ', task)
+        print(i,'.', task)
         i += 1
 
+
+
+userTask = input("1. Add Task\n2. Remove Task\n3. View Tasks\n4. View Daily task %\n")
 
 while True:
     userTask = input("Enter a task: ") 
